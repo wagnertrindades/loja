462c0fe831eec75e16e024ea1f14818368758563
wtrindades <wtrindades@gmail.com>
2022-01-05 11:05:38 -0300

    test: add CreateProduct tests

---
+++ src/main/java/br/com/alura/loja/tests/CreateProduct.java
@@ -0,0 +1,22 @@
+package br.com.alura.loja.tests;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.alura.loja.model.Product;
+
+public class CreateProduct {
+	public static void main(String[] args) {
+		Product smartphone = new Product();
+		smartphone.setName("IPhone 26");
+		smartphone.setDescription("Apple smartphone with virtual reality");
+		smartphone.setPrice(new BigDecimal("700"));
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
+		EntityManager em = factory.createEntityManager();
+		em.persist(smartphone);
+	}
+}
